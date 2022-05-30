9127deeda1372ce19a27bc5341b99f95f05a27d7
mhdi-nzari <mahdi.nazari.gb008@gmail.com>
2022-05-30 12:56:12 +0430

    New Upload By Ditgiz Project Name - Shop & Services

--- public/js/script.js
+++ public/js/script.js
@@ -4,33 +4,33 @@
 
 // console.log(p_tag);
 
-$(".js-range-slider").ionRangeSlider({
-  type: "single",
-  min: 1900000,
-  max: 59312000,
-  from: 40000000,
-  to: 59312000,
-  
-  keyboard:true,
-  grid_num:4,
-  grid: true,
-  to_fixed:true,
-  from_shadow:true,
-  prettify_separator:"،",
-  prettify_enabled:true,
+// const day = new Date(),
+// option = {
+//      weekday : 'long',
+//      year : 'numeric',
+//      month: 'long',
+//      day
+//
+// }
 
-});
+counter();
+function counter() {
+  let More = document.querySelectorAll(".More"),
+    Less = document.querySelectorAll(".less"),
+    counters = document.querySelector("#counter");
 
-
-const day = new Date(),
-option = {
-     weekday : 'long',
-     year : 'numeric',
-     month: 'long',
-     day:'
-     '
+  More.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      if (counters.value < 99) {
+        counters.value++;
+      }
+    });
+  });
+  Less.forEach((element) => {
+    element.addEventListener("click", () => {
+      if (counters.value > 0) {
+        counters.value--;
+      }
+    });
+  });
 }
-
-console.log(day.toLocaleString('fa-IR' , option));
-
- 
